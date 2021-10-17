b2e6d4b03385515a038dde95412d456331d069b2
Akarsh-J <76930147+Akarsh-J@users.noreply.github.com>
2021-10-17 21:59:56 +0530

    Display Grade

---
+++ A2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void main()
+{
+    int marks;
+    char grade;
+
+    printf("\n Enter your marks: ");
+    scanf("%d",&marks);
+
+    if(marks>=85 && marks<=100)
+        grade = 'A';
+    
+    else if(marks>=70 && marks<=84)
+        grade = 'B';
+    
+    else if(marks>=55 && marks<=69)
+        grade = 'C';
+    
+    else if(marks>=40 && marks<=54)
+        grade = 'D';
+    
+    else if(marks<=40)
+        grade = 'F';
+
+    printf("\n Grade secured : %c",grade);
+}
