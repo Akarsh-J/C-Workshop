2af665ac0f24eb1bac4e4735cd3810c2ef873104
Akarsh-J <76930147+Akarsh-J@users.noreply.github.com>
2021-10-30 22:27:14 +0530

    FILE HANDLING\

---
+++ A10.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+    FILE *fin;
+    char ch;
+    int i,pos;
+    fin=fopen("abc.txt","r+");
+    if(fin==NULL)
+    {
+        printf("Unable to open file.");
+    }
+
+    fseek(fin,0,SEEK_END);
+    pos=ftell(fin);
+
+    i=0;
+    while(i<pos)
+    {
+        i++;
+        fseek(fin,-i,SEEK_END);
+        ch=fgetc(fin);
+        printf("%c",ch);
+    }
+
+    //copy contents from one file to another
+
+    FILE *fout = fopen("op.txt","w");
+    if(fout==NULL)
+    {
+        printf("Unable to open file.");
+    }
+
+    else{
+        ch = fgetc(fin);
+        while (ch != EOF)
+        {
+            /* Write to destination file */
+            fputc(ch, fout);
+
+            /* Read next character from source file */
+            ch = fgetc(fin);
+        }
+    }     
+}
